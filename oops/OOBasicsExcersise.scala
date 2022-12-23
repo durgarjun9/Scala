0a6bb27b15dfe3e677bda21351f8ff46b6e31d6c
Durga Prasad Lakumarapu <ldurgap@Durgas-MacBook-Air.local>
2022-12-23 18:03:28 +0530

    Formatting OOBasicExcercise.scala

--- oops/OOBasicsExcersise.scala
+++ oops/OOBasicsExcersise.scala
@@ -10,48 +10,40 @@ object OOBasicsExcersise extends App{
       *     -  method: authorAge -> Age of the author @ year of release.
       *     -  method: isWrittenBy(author)
       *     -  method: copy (new year of release) - return new Instance of Novel with new year of release.
-      */
+    */
 
 
-      class Writer(firstName: String, surName: String, val yearOfBirth: Int){
+    class Writer(firstName: String, surName: String, val yearOfBirth: Int){
         
         def getFullName(): String = s"$surName $firstName"
 
-      }
-
-      class Novel(name: String, var yearOfRelease: Int, author: Writer){
-
+    }
+    
+    class Novel(name: String, var yearOfRelease: Int, author: Writer){
         def authorAge(): Int = 2022 - author.yearOfBirth
-
         def isWrittenBy(): String = author.getFullName()
-
         def copy(newYear: Int): Novel = {
             val novel = new Novel(this.name, newYear, author)
             novel
         }
-
-      }
-
-      val author: Writer = new Writer("Durga Prasad", "Lakumarapu", 1998)
-      var novel: Novel = new Novel("My Life as Dev", 2020, author)
-
-      println(novel.authorAge())
-      println(novel.isWrittenBy())
-      println(novel.yearOfRelease)
-      novel = novel.copy(2022)
-      println(novel.yearOfRelease)
-
-
-
-
-
-      /**
-        * Counter Class
+    }
+    
+    val author: Writer = new Writer("Durga Prasad", "Lakumarapu", 1998)
+    var novel: Novel = new Novel("My Life as Dev", 2020, author)
+    println(novel.authorAge())
+    println(novel.isWrittenBy())
+    println(novel.yearOfRelease)
+    novel = novel.copy(2022)
+    println(novel.yearOfRelease)
+  /**
+    * Counter Class
         * - receives an Int value
         * method: returns currentCount()
         * method: Increment/ Decrement by 1 => new Counter
         * overload: inc / dec to receive a parameter by which increment and decrement will be done. and return new Counter
         */
+
+      
 
 
 
