27d0ecb2168d4596e8bd6b52d8e4638b98e2c8db
Durga Prasad Lakumarapu <durgarjun9@gmail.com>
2022-12-25 19:10:00 +0530

    Init Notation or Operator Notation

--- oops/MethodNotations.scala
+++ oops/MethodNotations.scala
@@ -1,1 +1,22 @@
-object MethodNotations extends App{}
+object MethodNotations extends App{
+
+    class Person(val name: String, favMovie: String){
+        def likes(movie: String): Boolean = movie == favMovie
+
+        def hangOutWith(person: Person): String = s"This $name is hanging out with ${person.name}"
+    }
+
+    val dp = new Person("Durga Prasad", "RRR")
+    // Calling likes methods using dp object.
+    println(dp.likes("RRR"))
+    // Different way of calling, likes method using dp object. Both above and below are equivalent.
+    println(dp likes "RRR")
+    // Above is called Infix Notation or Operator Notation. It only works with methods which have only one parameter.
+    // object method parameter
+
+
+    // "operators" in scala.
+    println(dp.hangOutWith(new Person("AK", "Avatar 2")))
+    
+
+}
