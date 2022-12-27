6c02a638be016acfc0a1aaa1dadaeca193347ba6
Durga Prasad Lakumarapu <durgarjun9@gmail.com>
2022-12-27 23:32:35 +0530

    writing Anonymous class for defined classes

--- oops/AnonymousClasses.scala
+++ oops/AnonymousClasses.scala
@@ -12,5 +12,12 @@ object AnonymousClasses extends App{
     
     println(funnyAnimal.getClass())
 
+    class Person(name: String){
+        def sayHi: Unit = println(s"Hi, my name is $name, how can I help")
+    }
+
+    val jim = new Person("Jim") {
+        override def sayHi: Unit = println("Hi, my name is Jim. How may I assist you.")
+    }
 
 }
