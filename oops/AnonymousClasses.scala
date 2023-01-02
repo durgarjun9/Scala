3827c28192bddd558f0213cf3a6e071eb92a6498
Durga Prasad Lakumarapu <durgarjun9@gmail.com>
2023-01-02 12:29:00 +0530

    Some Comments in AnnonymousClasses

--- oops/AnonymousClasses.scala
+++ oops/AnonymousClasses.scala
@@ -6,6 +6,7 @@ object AnonymousClasses extends App{
     }
 
     // Anonymous class, we wrote new class implemention while intializing the class.
+    // Internally Scala creates a class for the anonymus class and converts the line 10 to val funnyAnimal: Animal = new AnnonymusClasses$$anony$1
     val funnyAnimal: Animal = new Animal {
         override def eat: Unit = println("hahahahha")
     }
@@ -15,7 +16,7 @@ object AnonymousClasses extends App{
     class Person(name: String){
         def sayHi: Unit = println(s"Hi, my name is $name, how can I help")
     }
-
+    // Definetly need to pass the parameters if the original class has the parameters in it. else its fine.
     val jim = new Person("Jim") {
         override def sayHi: Unit = println("Hi, my name is Jim. How may I assist you.")
     }
